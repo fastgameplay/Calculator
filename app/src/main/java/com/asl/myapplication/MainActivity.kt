401fa3cad1ecd50839d87ad36b4aac772411348a
fastgameplay <61421145+fastgameplay@users.noreply.github.com>
2021-10-28 21:56:37 +0400

    Whats 9 + 10

--- app/src/main/java/com/asl/myapplication/MainActivity.kt
+++ app/src/main/java/com/asl/myapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package com.asl.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asl.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+//        binding.textMain.text = "f"
+
+//        binding.btnBackspace.setOnClickListener()
     }
 }
