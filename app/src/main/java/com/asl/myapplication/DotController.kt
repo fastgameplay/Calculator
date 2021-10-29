8b4c0709ae223080e0f4077c8a334d7d7a8ce3ab
fastgameplay <61421145+fastgameplay@users.noreply.github.com>
2021-10-29 22:46:37 +0400

    String splitter to basic tokens

--- app/src/main/java/com/asl/myapplication/DotController.kt
+++ app/src/main/java/com/asl/myapplication/DotController.kt
@@ -3,25 +3,25 @@ package com.asl.myapplication
 class DotController {
     //2 cups of tea and its working
     //if last member of list is true -> placing dot is possible
-    private val IsPossible: MutableList<Boolean> = mutableListOf(true)
+    private val IsPossible: ArrayList<Boolean> = arrayListOf(true)
 
-    public fun CheckId(value : Int) : Boolean{
+    fun CheckId(value : Int) : Boolean{
         return IsPossible[value]
     }
-    public fun CheckForLast() : Boolean{
+    fun CheckForLast() : Boolean{
         return IsPossible.last()
     }
-    public fun GotoNext(){
+    fun GotoNext(){
         IsPossible.add(true)
     }
-    public fun GotoLast(){
+    fun GotoLast(){
         if(IsPossible.count() > 1)
             IsPossible.removeLast()
     }
-    public fun SetLastDot(value :Boolean){
+    fun SetLastDot(value :Boolean){
         IsPossible[IsPossible.lastIndex] = value
     }
-    public fun Clear(){
+    fun Clear(){
         IsPossible.clear()
         IsPossible.add(true)
     }
